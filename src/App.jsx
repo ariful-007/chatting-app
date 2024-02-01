592bc6b3ae1333488ab83e013bd76b36cc9d5b99
ariful007 <arifulislamwd007@gmail.com>
2024-02-01 13:17:29 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import './App.css'
 import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import Main from './Layout/Main';
 import Registration from './Layout/Registration';
 import Login from './Layout/Login';
 
 function App() {
   const router = createBrowserRouter([
-    {
-      path:'/',
-      element:<Main/>
-    },
     {
       path:'/registration',
       element:<Registration/>
--- src/Layout/Registration.jsx
+++ src/Layout/Registration.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { IoEyeOff ,IoEye } from "react-icons/io5";
 
 const Registration = () => {
 // input infometion start
@@ -16,30 +18,51 @@ const [passwordError, setPasswordError] = useState("")
 const [rePasswordError, setRePasswordError] = useState("")
 // input field error messeage end
 
+const [showIcon , setShowIcon] = useState(false)
+
+const heandelShowIcon = () =>{
+  setShowIcon(!showIcon)
+}
+
 // get input value start
 const heandelFullName = (e) =>{
   setFullName(e.target.value)
+  setFullNameError("")
 }
 
 const heandelEmail = (e) =>{
   setEmail(e.target.value)
+  setEmailError("")
   
 }
 
 const heandelPassword = (e) =>{
   setPassword(e.target.value)
+  setPasswordError("")
   
 }
 
 const heandelRePassword = (e) =>{
   setRePassword(e.target.value)
-  
+  setRePasswordError("")
 }
 // get input value end
 
 // registration submit start
 const heandelSubmit = (e) => {
   e.preventDefault()
+  if(!fullName){
+    setFullNameError("FullName is Required")
+  }
+  if(!email){
+    setEmailError("Email is Required")
+  }
+  if(!password){
+    setPasswordError("Password is Required")
+  }
+  if(!rePassword){
+    setRePasswordError("Re-password is Required")
+  }
 }
 // registration submit end
 
@@ -54,19 +77,39 @@ const heandelSubmit = (e) => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm registratin_form">
         <form onSubmit={heandelSubmit}>
               <input type="text" onChange={heandelFullName}  value={fullName} placeholder="Full Name"/>
+              <p className="error_message">{fullNameError}</p>
               <input type="email" onChange={heandelEmail} value={email} placeholder="Email" />
-              <input type="password" onChange={heandelPassword}  value={password} placeholder="Password"/>
-              <input type="password" onChange={heandelRePassword} value={rePassword} placeholder="Re-enter Password"/>
+              <p className="error_message">{emailError}</p>
+              <div className="relative">
+                <input type={showIcon ? "text" : "password"} onChange={heandelPassword}  value={password} placeholder="Password"/>
+                {
+                  showIcon ? 
+                  <IoEye onClick={heandelShowIcon}  className=" icon_passwor" />
+                  :
+                  <IoEyeOff onClick={heandelShowIcon} className=" icon_passwor" />
+                }
+              </div>
+              <p className="error_message">{passwordError}</p>
+              <div className="relative">
+              <input type={showIcon ? "text" : "password"} onChange={heandelRePassword} value={rePassword} placeholder="Re-enter Password"/>
+                {
+                  showIcon ? 
+                  <IoEye onClick={heandelShowIcon}  className=" icon_passwor" />
+                  :
+                  <IoEyeOff onClick={heandelShowIcon} className=" icon_passwor" />
+                }
+              </div>
+              <p className="error_message">{rePasswordError}</p>
               <button type="submit" className=" Button_v_1">Sign in</button>
         </form>
         <p className="mt-10 text-center text-sm text-black">
-          Not a member?{" "}
-          <a
-            href="#"
+        You already have an account? Please {" "}
+          <Link
+            to='/login'
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
-            Start a 14 day free trial
-          </a>
+            Login
+          </Link>
         </p>
       </div>
     </div>
