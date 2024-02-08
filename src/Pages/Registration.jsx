1944bceebdfa1442838c76304b18c84c5a2a4225
ariful007 <arifulislamwd007@gmail.com>
2024-02-09 00:46:04 +0600

    new update

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,137 +1,193 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { ThreeDots } from 'react-loader-spinner'
-import { IoEye,IoEyeOff } from "react-icons/io5";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ThreeDots } from "react-loader-spinner";
+import { IoEye, IoEyeOff } from "react-icons/io5";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Registration = () => {
   const auth = getAuth();
   const navigate = useNavigate();
   // gate input start
-  const [fullName, setFullName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [repassword, setRepassword] = useState("")
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
   // gate input end
   // gate input error start
-  const [fulNameError, setFullNameError] = useState("")
-  const [emailError, setEmailError] = useState("")
-  const [passwordError, setPasswordError] = useState("")
-  const [repasswordError, setRepasswordError] = useState("")
+  const [fulNameError, setFullNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [repasswordError, setRepasswordError] = useState("");
   // gate input error end
   // gate regex start
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   const nameRegex = /^[a-zA-Z]+$/;
   // gate regex end
   // gate loader start
-  const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState(false);
   // gate loader end
   // gate icon start
-  const [showIcon, setShowIcon] = useState(false)
-  const handelShowIcon = () =>{
-    setShowIcon(!showIcon)
-  }
+  const [showIcon, setShowIcon] = useState(false);
+  const handelShowIcon = () => {
+    setShowIcon(!showIcon);
+  };
   // gate icon end
 
   // gate input function start
   const handleFullName = (e) => {
-    setFullName(e.target.value)
-    setFullNameError("")
-  }
+    setFullName(e.target.value);
+    setFullNameError("");
+  };
   const handleEmail = (e) => {
-    setEmail(e.target.value)
-    setEmailError("")
-  }
+    setEmail(e.target.value);
+    setEmailError("");
+  };
   const handlePassword = (e) => {
-    setPassword(e.target.value)
-    setPasswordError("")
-  }
+    setPassword(e.target.value);
+    setPasswordError("");
+  };
   const handleRepassword = (e) => {
-    setRepassword(e.target.value)
-    setRepasswordError("")
-  }
-  // gate input function end 
+    setRepassword(e.target.value);
+    setRepasswordError("");
+  };
+  // gate input function end
   const handelSubmit = (e) => {
-    e.preventDefault()
-    if(!fullName){
-      setFullNameError("FullName is Required")
-    }else if(!nameRegex.test(fullName)){
-      setFullNameError("FullName is Invalid")
+    e.preventDefault();
+    if (!fullName) {
+      setFullNameError("FullName is Required");
+    } else if (!nameRegex.test(fullName)) {
+      setFullNameError("FullName is Invalid");
     }
-    if(!email){
-      setEmailError("Email is Required")
+    if (!email) {
+      setEmailError("Email is Required");
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Invalid Email");
     }
-    else if(!emailRegex.test(email)){
-      setEmailError("Invalid Email")
+    if (!password) {
+      setPasswordError("Password is Required");
     }
-    if(!password){
-      setPasswordError("Password is Required")
+    if (!repassword) {
+      setRepasswordError("Repassword is Required");
     }
-    if(!repassword){
-      setRepasswordError("Repassword is Required")
+    if (password !== repassword) {
+      setRepasswordError("Password and Re-password not Match");
     }
-    if(password !== repassword){
-      setRepasswordError("Password and Re-password not Match")
-    } 
-    if(fullName && nameRegex.test(fullName) && email && emailRegex.test(email) && password && repassword && password === repassword){
-      setLoader(true)
+    if (
+      fullName &&
+      nameRegex.test(fullName) &&
+      email &&
+      emailRegex.test(email) &&
+      password &&
+      repassword &&
+      password === repassword
+    ) {
+      setLoader(true);
       createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        toast.success("Registration successfully")
-        navigate("/login")
-        setLoader(false)
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      }); 
+        .then((userCredential) => {
+          updateProfile(auth.currentUser, {
+            displayName: fullName,
+            photoURL: "https://example.com/jane-q-user/profile.jpg",
+          })
+            .then(() => {
+              const user = userCredential.user;
+              console.log(user);
+              toast.success("Registration successfully");
+              navigate("/login");
+              setLoader(false);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
     }
-  }
+  };
 
   return (
     <div id="registration_form">
       <div className="main_menu">
         <h2>Registration Form</h2>
         <form onSubmit={handelSubmit}>
-            <input onChange={handleFullName} value={fullName} type="text" placeholder="Full Name" />
-            <p className="error_message">{fulNameError}</p>
-            <input onChange={handleEmail} value={email} type="text" placeholder="Email" />
-            <p className="error_message">{emailError}</p>
-            <div className=" relative">
-            <input onChange={handlePassword} value={password} type={showIcon ? "text" : "password"} placeholder="Password" />
-            {
-              showIcon?
-              <IoEye onClick={handelShowIcon} className=" absolute right-4 cursor-pointer  top-[64%] translate-y-[-50%]" />
-              :
-              <IoEyeOff onClick={handelShowIcon} className=" absolute right-4 cursor-pointer top-[64%] translate-y-[-50%]" />
-            }
-            </div>
-            <p className="error_message">{passwordError}</p>
-            <div className=" relative">
-            <input onChange={handleRepassword} value={repassword} type={showIcon ? "text" : "password"} placeholder="Re-Password" />
-            {
-              showIcon?
-              <IoEye onClick={handelShowIcon} className=" absolute right-4 cursor-pointer  top-[64%] translate-y-[-50%]" />
-              :
-              <IoEyeOff onClick={handelShowIcon} className=" absolute right-4 cursor-pointer top-[64%] translate-y-[-50%]" />
-            }
-            </div>
-            <p className="error_message">{repasswordError}</p>
+          <input
+            onChange={handleFullName}
+            value={fullName}
+            type="text"
+            placeholder="Full Name"
+          />
+          <p className="error_message">{fulNameError}</p>
+          <input
+            onChange={handleEmail}
+            value={email}
+            type="text"
+            placeholder="Email"
+          />
+          <p className="error_message">{emailError}</p>
+          <div className=" relative">
+            <input
+              onChange={handlePassword}
+              value={password}
+              type={showIcon ? "text" : "password"}
+              placeholder="Password"
+            />
+            {showIcon ? (
+              <IoEye
+                onClick={handelShowIcon}
+                className=" absolute right-4 cursor-pointer  top-[64%] translate-y-[-50%]"
+              />
+            ) : (
+              <IoEyeOff
+                onClick={handelShowIcon}
+                className=" absolute right-4 cursor-pointer top-[64%] translate-y-[-50%]"
+              />
+            )}
+          </div>
+          <p className="error_message">{passwordError}</p>
+          <div className=" relative">
+            <input
+              onChange={handleRepassword}
+              value={repassword}
+              type={showIcon ? "text" : "password"}
+              placeholder="Re-Password"
+            />
+            {showIcon ? (
+              <IoEye
+                onClick={handelShowIcon}
+                className=" absolute right-4 cursor-pointer  top-[64%] translate-y-[-50%]"
+              />
+            ) : (
+              <IoEyeOff
+                onClick={handelShowIcon}
+                className=" absolute right-4 cursor-pointer top-[64%] translate-y-[-50%]"
+              />
+            )}
+          </div>
+          <p className="error_message">{repasswordError}</p>
 
-            { 
-              loader ?
-              <div className=" flex justify-center"><ThreeDots  color="#000" /></div>
-              :
-              <button type="submit"className="Button_v_1"> Register </button>
-            }
-
+          {loader ? (
+            <div className=" flex justify-center">
+              <ThreeDots color="#000" />
+            </div>
+          ) : (
+            <button type="submit" className="Button_v_1">
+              {" "}
+              Register{" "}
+            </button>
+          )}
         </form>
         <p className="menu_text">
-            You already have an account? Please {" "}
-          <Link to='/login' className="menu_btn" >Login</Link>
+          You already have an account? Please{" "}
+          <Link to="/login" className="menu_btn">
+            Login
+          </Link>
         </p>
       </div>
     </div>
--- src/home/Navbar.jsx
+++ src/home/Navbar.jsx
@@ -2,14 +2,20 @@
 import { Link, useNavigate } from "react-router-dom";
 import { IoHomeSharp,IoChatboxEllipses,IoNotifications,IoLogOut } from "react-icons/io5";
 import { getAuth, signOut } from "firebase/auth";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userLoginInfo } from '../slice/userSlice';
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { useState } from "react";
 
 
 const Navbar = () => {
   const auth = getAuth();
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const [showModal, setShowModal] = useState()
+
+  const data = useSelector((state) => state.userLoginInfo.userInfo)
+  console.log(data)
 
   const heandelLogout = () => {
     signOut(auth).then(() => {
@@ -25,10 +31,14 @@ const Navbar = () => {
     <nav id="navbar" className="bg-gray-700 py-2 px-4">
       <div className="main_navbar">
         <div className="navbar_img">
-          <div className="navbar_img_round">
-            <img src="/" alt="" />
+          <div className="navbar_img_round relative group">
+            <img src='' alt="" />
+            <h1 className="displayImg group-hover:opacity-0">{data?.displayName[0]}</h1>
+            <div className="overlay hidden group-hover:block">
+            <FaCloudUploadAlt />
+            </div>
           </div>
-          <h1>Ariful Islam</h1>
+          <h1>{data?.displayName}</h1>
         </div>
         <div className="nav_icon_menu">
           <div className="nav_box">
@@ -44,6 +54,17 @@ const Navbar = () => {
           <Link onClick={heandelLogout} to='/login' className="nav_icon"> <IoLogOut  /></Link>
           </div>
         </div>
+      </div>
+      <div className="modal">
+        <div className="profileImages">
+          <h1>Update Your Profile Picture </h1>
+          <input className=" my-4" type="file" />
+          <div className=" flex gap-7">
+          <button className="Button_v_2 py-2 px-8">Uplode</button>
+          <button className="Button_v_3 py-2 px-8">cansel</button>
+          </div>
+        </div>
+        
       </div>
     </nav>
   );
