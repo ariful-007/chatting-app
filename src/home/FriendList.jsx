7e744de00cf5584b8c8f04010afe4bc7a93de1b6
ariful007 <arifulislamwd007@gmail.com>
2024-02-18 01:02:07 +0600

    new update

--- src/home/FriendList.jsx
+++ src/home/FriendList.jsx
@@ -1,33 +1,61 @@
-import React from 'react';
-import { Button_v_2, Button_v_3 } from '../componentes/Button';
-import { BiDotsVerticalRounded } from 'react-icons/bi';
+import React, { useEffect, useState } from "react";
+import { Button_v_2, Button_v_3 } from "../componentes/Button";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { useSelector } from "react-redux";
+import ProfilePicture from "../componentes/ProfilePicture";
 
 const FriendList = () => {
+  const db = getDatabase();
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
+  const [friendList, setFriendList] = useState([]);
+  console.log(friendList);
+
+  useEffect(() => {
+    const friendRef = ref(db, "friend");
+    onValue(friendRef, (snapshot) => {
+      const list = [];
+      snapshot.forEach((item) => {
+        if (
+          data.uid == item.val().receverId ||
+          data.uid == item.val().senderId
+        ) {
+          list.push({ ...item.val(), id: item.key });
+        }
+      });
+      setFriendList(list);
+    });
+  }, []);
+
   return (
     <div id="all-item">
       <div className="main">
         <h2>Friend List</h2>
-        <p> <BiDotsVerticalRounded /> </p>
+        <p>
+          {" "}
+          <BiDotsVerticalRounded />{" "}
+        </p>
       </div>
-      <div className="flex justify-between items-center p-2">
-        <div className=" flex gap-5">
-          <div className="img">
-            <img src="" alt="" />
+      {friendList.map((item) => {
+        return (
+          <div key={item.id} className="flex justify-between items-center p-2">
+            <div className=" flex gap-5">
+              <div className="img">
+                <ProfilePicture imgId={item.senderId} />
+              </div>
+              <div>
+                <h1>{item.senderName}</h1>
+              </div>
+            </div>
+            <div className=" flex gap-3">
+              <Button_v_3>Block</Button_v_3>
+              <Button_v_2>UnFriend</Button_v_2>
+            </div>
           </div>
-          <div>
-            <h1>Group Name</h1>
-            <p>Hi Guys,Wassup!</p>
-          </div>
-        </div>
-        <div className=' flex gap-3'>
-        <Button_v_3>Block</Button_v_3>
-        <Button_v_2>UnFriend</Button_v_2>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
-
-      
 
 export default FriendList;
--- src/home/FriendRequest.jsx
+++ src/home/FriendRequest.jsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from "react";
 import { Button_v_2, Button_v_3 } from "../componentes/Button";
 import { BiDotsVerticalRounded } from "react-icons/bi";
-import { getDatabase, onValue, ref} from "firebase/database";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
 import { useSelector } from "react-redux";
 import ProfilePicture from "../componentes/ProfilePicture";
 
 const FriendRequest = () => {
   const db = getDatabase();
-  const data = useSelector((state) => state.userLoginInfo.userInfo)
-  let [friendRequestList, setFriendRequestList] = useState([]);
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
+  const [friendRequestList, setFriendRequestList] = useState([]);
   console.log(friendRequestList);
 
   useEffect(() => {
     const friendRequestRef = ref(db, "friendRequest");
-    
+
     onValue(friendRequestRef, (snapshot) => {
       const list = [];
       snapshot.forEach((item) => {
-        if(item.val().receverId === data.uid){
-          list.push({...item.val(), id: item.key})
+        if (item.val().receverId === data.uid) {
+          list.push({ ...item.val(), id: item.key });
         }
       });
       setFriendRequestList(list);
-      
     });
-    
   }, []);
+
+  const headelFriendRequest = (item) => {
+    set(
+      push(ref(db, "friend"), {
+        ...item,
+      }).then(() => {
+        remove(ref(db, "friendRequest/" + item.id));
+      })
+    );
+  };
 
   return (
     <div id="all-item">
@@ -36,26 +51,26 @@ const FriendRequest = () => {
           <BiDotsVerticalRounded />{" "}
         </p>
       </div>
-      {
-         friendRequestList.map((item)=>{
-          return(
-            <div key={item.id} className="flex justify-between items-center p-2">
-          <div className=" flex gap-5">
-            <div className="img">
-              <ProfilePicture imgId={item.senderId}/>
+      {friendRequestList.map((item) => {
+        return (
+          <div key={item.id} className="flex justify-between items-center p-2">
+            <div className=" flex gap-5">
+              <div className="img">
+                <ProfilePicture imgId={item.senderId} />
+              </div>
+              <div>
+                <h1>{item.senderName}</h1>
+              </div>
             </div>
-            <div>
-              <h1>{item.senderName}</h1>
+            <div className=" flex gap-2">
+              <div onClick={() => headelFriendRequest(item)}>
+                <Button_v_2>Accept</Button_v_2>
+              </div>
+              <Button_v_3>Decline</Button_v_3>
             </div>
           </div>
-          <div className=" flex gap-2">
-            <Button_v_2>Accept</Button_v_2>
-            <Button_v_3>Decline</Button_v_3>
-          </div>
-        </div>
-          )
-        })
-      }
+        );
+      })}
     </div>
   );
 };
