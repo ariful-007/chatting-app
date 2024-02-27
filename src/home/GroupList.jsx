7e25179b00532aa826045178795a6dc59a32331a
ariful007 <arifulislamwd007@gmail.com>
2024-02-27 23:57:37 +0600

    new update

--- src/home/GroupList.jsx
+++ src/home/GroupList.jsx
@@ -1,13 +1,68 @@
-import React from "react";
-import { BiDotsVerticalRounded } from "react-icons/bi";
+import {  getDatabase, push, ref, set } from "firebase/database";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from 'react-toastify';
+
 
 const GroupList = () => {
+  const db = getDatabase()
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
+
+  const [show, setShow]= useState(false)
+  const [groupName, setGroupName]= useState('')
+  const [gropIntro, setGropIntro]= useState('')
+  const [groupNameError, setGroupNameError] = useState('')
+  const [gropIntroError, setGropIntroError] = useState('')
+
+  const heandelGroupName =(e)=>{
+    setGroupName(e.target.value)
+    setGroupNameError('')
+  }
+  const heandelGropIntro =(e)=>{
+    setGropIntro(e.target.value)
+    setGropIntroError('')
+  }
+
+  const heandelCreateGroup = ()=>{
+    if(groupName== ''){
+      setGroupNameError('Group name is required')
+    }
+    else if(gropIntro==''){
+      setGroupNameError('Group name must be at least 3 characters')
+    }
+    else{
+
+      set(push(ref(db, "group")),{
+        groupName: groupName,
+        gropIntro: gropIntro,
+        adminName: data.displayName,
+        adminId: data.uid
+      }).then(()=>{
+        toast.success("Group Created")
+      setShow(false)
+      setGroupName('')
+      setGropIntro('')
+      })
+    }
+  }
+
+
   return (
     <div id="all-item">
       <div className="main">
         <h2>Group List</h2>
-        <p> <BiDotsVerticalRounded /> </p>
-        </div>
+        <button onClick={()=> setShow(!show)} className="Button_v_3">{show? "Cencel":"Create group"}</button>
+      </div>
+      {
+        show?
+        <div className=" p-4 bg-blue-500 rounded-lg">
+        <input onChange={heandelGroupName} type="text" placeholder="Group Name"  className=" outline-none p-2 w-full rounded-lg py-1"/>
+        <p className=" text-red-500">{groupNameError}</p>
+        <input onChange={heandelGropIntro} type="text" placeholder="Group Intro" className=" outline-none p-2 w-full rounded-lg py-1 mt-3"/>
+        <p className=" text-red-500">{gropIntroError}</p>
+        <button onClick={heandelCreateGroup} className="Button_v_3 w-full">Create</button>
+      </div>
+      :
       <div className="flex justify-between items-center p-2">
         <div className=" flex gap-5">
           <div className="img">
@@ -20,7 +75,8 @@ const GroupList = () => {
         </div>
         <button className=" Button_v_2">join</button>
       </div>
-      
+
+      }
     </div>
   );
 };
--- src/home/MyGroup.jsx
+++ src/home/MyGroup.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { getDatabase, onValue, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { useSelector } from "react-redux";
 
 const MyGroup = () => {
+  const db = getDatabase();
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
+  const [groupList, setGroupList] = useState([]);
+
+  const groupRef = ref(db, "group");
+  useEffect(() => {
+    onValue(groupRef, (snapshot) => {
+      const list = [];
+      snapshot.forEach((item) => {
+        if (data.uid == item.val().adminId) {
+          list.push({ ...item.val(), id: item.key });
+        }
+      });
+      setGroupList(list);
+    });
+  }, []);
+
   return (
     <div id="all-item">
       <div className="main">
         <h2>My Group</h2>
-        <p> <BiDotsVerticalRounded /> </p>
+        <p>
+          {" "}
+          <BiDotsVerticalRounded />{" "}
+        </p>
       </div>
-      <div className="flex justify-between items-center p-2">
-        <div className=" flex gap-5">
-          <div className="img">
-            <img src="" alt="" />
-          </div>
-          <div>
-            <h1>Group Name</h1>
-            <p>Hi Guys,Wassup!</p>
-          </div>
-        </div>
-        <button className=" Button_v_2">Block</button>
-      </div>
+      {
+        groupList.length == 0?
+        <h1 className=" text-xl text-black font-bold text-center mt-3">No Group available</h1>
+        :
+        groupList.map((item, i) => {
+          return (
+            <div key={i} className="flex justify-between items-center p-2">
+              <div className=" flex gap-5">
+                <div className="img flex justify-center items-center">
+                  <h1>{item.groupName[0]}</h1>
+                </div>
+                <div>
+                  <h1>{item.adminName}</h1>
+                  <p className=" text-base font-bold text-black">
+                    {item.gropIntro}
+                  </p>
+                </div>
+              </div>
+              <button className=" Button_v_2">Info</button>
+              <button className=" Button_v_3">Request</button>
+              <button className=" Button_v_2">Delete</button>
+            </div>
+          );
+        })
+      }
+      
     </div>
   );
 };
